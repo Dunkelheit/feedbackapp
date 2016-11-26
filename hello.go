bc0f5d3936d053bb2eb4cb591ee4e58e9a0fe4b5
Arturo Martinez Minguez <arturo@icemobile.com>
2016-11-26 21:25:26 +0100

    feat: start working on the controllers

--- hello.go
+++ hello.go
@@ -11,6 +11,8 @@ import (
 	"gopkg.in/gin-gonic/gin.v1"
 )
 
+var o orm.Ormer
+
 func ping(c *gin.Context) {
 	card := model.Card{ID: 1, Title: "Hello"}
 	c.JSON(http.StatusOK, card)
@@ -22,14 +24,14 @@ func main() {
 	userRoutes := router.Group("/users")
 	{
 		userRoutes.POST("", ping)
-		userRoutes.GET("/:userId", ping)
+		userRoutes.GET("/:userId", UserById)
 		userRoutes.PUT("/:userId", ping)
 		userRoutes.DELETE("/:userId", ping)
 	}
 
 	cardRoutes := router.Group("/cards")
 	{
-		cardRoutes.GET("", ping)
+		cardRoutes.GET("", AllCards)
 		cardRoutes.POST("", ping)
 		cardRoutes.PUT("/:cardId", ping)
 		cardRoutes.DELETE("/:cardId", ping)
@@ -55,4 +57,21 @@ func init() {
 	if err != nil {
 		fmt.Println(err)
 	}
+	o = orm.NewOrm()
+	o.Using("default")
+
+	cards := []model.Card{
+		{Title: "Great moves", Category: model.CardCategoryPositive},
+		{Title: "Computer hacking skills", Category: model.CardCategoryPositive},
+		{Title: "Lazy eye", Category: model.CardCategoryNegative},
+		{Title: "Stinky feet", Category: model.CardCategoryNegative},
+	}
+	users := []model.User{
+		{Name: "Arturo Martinez", Email: "arturo@icemobile.com", JobTitle: "Node.js Developer", Avatar: "none"},
+		{Name: "Rosa van Colmjon", Email: "rosa@icemobile.com", JobTitle: "Scrum Master Extraordinaire", Avatar: "none"},
+		{Name: "Caio Borges", Email: "caio@icemobile.com", JobTitle: "Test Engineer", Avatar: "none"},
+		{Name: "Marco Silva", Email: "marco@icemobile.com", JobTitle: "Visual Designer", Avatar: "none"},
+	}
+	fmt.Println(o.InsertMulti(len(cards), cards))
+	fmt.Println(o.InsertMulti(len(users), users))
 }
