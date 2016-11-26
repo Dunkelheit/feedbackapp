0a68b7e2de717c2a502254c9ac266281f313cd68
Arturo Martinez Minguez <arturo@icemobile.com>
2016-11-26 18:26:52 +0100

    feat: initialize database

--- hello.go
+++ hello.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+	"database/sql"
+	"log"
+	"net/http"
+
+	"github.com/go-gorp/gorp"
+	_ "github.com/lib/pq"
+
 	"github.com/Dunkelheit/feedbackapp/model"
 	"gopkg.in/gin-gonic/gin.v1"
 )
 
 func ping(c *gin.Context) {
 	card := model.Card{ID: 1, Title: "Hello"}
-	c.JSON(200, card)
+	c.JSON(http.StatusOK, card)
 }
 
 func main() {
+
+	// initialize the DbMap
+	dbmap := initDb()
+	defer dbmap.Db.Close()
+
 	router := gin.Default()
 
 	userRoutes := router.Group("/users")
@@ -39,4 +51,33 @@ func main() {
 	}
 
 	router.Run() // listen and server on 0.0.0.0:8080
+}
+
+func initDb() *gorp.DbMap {
+	// connect to db using standard Go database/sql API
+	// use whatever database/sql driver you wish
+	db, err := sql.Open("postgres", "user=arturo.martinez dbname=feedbackapp sslmode=disable")
+	checkErr(err, "sql.Open failed")
+
+	// construct a gorp DbMap
+	dbmap := &gorp.DbMap{Db: db, Dialect: gorp.PostgresDialect{}}
+
+	// add a table, setting the table name to 'posts' and
+	// specifying that the Id property is an auto incrementing PK
+	dbmap.AddTableWithName(model.Card{}, "cards").SetKeys(true, "ID")
+	dbmap.AddTableWithName(model.User{}, "users").SetKeys(true, "ID")
+	dbmap.AddTableWithName(model.Review{}, "reviews").SetKeys(true, "ID")
+
+	// create the table. in a production system you'd generally
+	// use a migration tool, or create the tables via scripts
+	err = dbmap.CreateTablesIfNotExists()
+	checkErr(err, "Create tables failed")
+
+	return dbmap
+}
+
+func checkErr(err error, msg string) {
+	if err != nil {
+		log.Fatalln(msg, err)
+	}
 }
