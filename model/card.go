0c36b8aa09a228fb7b6cd4f91c1fec9eb324047c
Arturo Martinez Minguez <arturo@icemobile.com>
2016-11-27 15:01:15 +0100

    fix: make title and category mandatory

--- model/card.go
+++ model/card.go
@@ -3,6 +3,6 @@ package model
 // Card represents a single card
 type Card struct {
 	ID       ID           `json:"id" orm:"auto"`
-	Title    string       `json:"title"`
-	Category CardCategory `json:"category"`
+	Title    string       `json:"title" binding:"required"`
+	Category CardCategory `json:"category" binding:"required"`
 }
